83f7b9a3d64aefd3e8fb672521a0eda93cf0e4db
PW <pwasows@gmail.com>
2018-06-08 00:22:33 +0200

    Implement document SVD vector space preparation

--- car_recommender/recommender_engine.py
+++ car_recommender/recommender_engine.py
@@ -1,6 +1,7 @@
 import random
 from nltk.tokenize import RegexpTokenizer
 import morfeusz
+from gensim import corpora, models, similarities, matutils
 
 
 class RecommenderEngine:
@@ -9,6 +10,13 @@ class RecommenderEngine:
         self._stopwords = self._load_stopwords()
 
     def find_similar(self, ad_hyperlink, ads_number):
+        model, corpus = self._prepare_vector_space_model()
+        similarity_matrix = self._prepare_similarity_matrix(LSI_model,
+                                                            tf_idf_corpus)
+        ad_row = self._ads_database.index.get_loc(ad_hyperlink)
+
+        #TODO: get ads_number most similar ads
+
         result = []
         for _ in range(0, ads_number):
             result.append(random.randint(1, 5))
@@ -21,6 +29,13 @@ class RecommenderEngine:
         ads_data = self._to_lowercase(ads_data)
         ads_data = self._remove_stopwords(ads_data)
         ads_data = self._lemmatize(ads_data)
+        dictionary = self._prepare_dictionary(ads_data)
+        corpus = self._prepare_corpus(ads_data, dictionary)
+        tf_idf_model = self._prepare_tf_idf_model(corpus)
+        tf_idf_corpus = self._prepare_tf_idf_corpus(corpus, tf_idf_model)
+        LSI_model = self._prepare_LSI_model(tf_idf_corpus, dictionary)
+        return LSI_model, tf_idf_model
+
 
     def _load_stopwords(self):
         stopwords = []
@@ -68,6 +83,24 @@ class RecommenderEngine:
             result.append(text_lemmas)
 
         return result
+
+    def _prepare_dictionary(self, texts):
+        return corpora.Dictionary(texts)
+
+    def _prepare_corpus(self, texts, dictionary):
+        return [dictionary.doc2bow(text) for text in texts]
+
+    def _prepare_tf_idf_model(self, corpus):
+        return models.TfidfModel(corpus)
+
+    def _prepare_tf_idf_corpus(self, corpus, tf_idf_model):
+        return tf_idf_model[corpus]
+
+    def _prepare_LSI_model(self, corpus, dictionary):
+        return models.LsiModel(corpus, id2word=dictionary, num_topics=30)
+
+    def _prepare_similarity_matrix(self, model, corpus):
+        return similarities.MatrixSimilarity(model[corpus])
 
     def _find_similar_ads(self, model, user_ad_indexes, number):
         pass
--- car_recommender/recommender_engine_test.py
+++ car_recommender/recommender_engine_test.py
@@ -1,4 +1,6 @@
 from recommender_engine import RecommenderEngine
+from gensim import corpora
+from time import time
 
 
 test_data = ''
@@ -19,5 +21,25 @@ data = engine._to_lowercase(data)
 data = engine._remove_stopwords(data)
 #print(data) # OK
 
+start = time()
 data = engine._lemmatize(data)
+print('lemmatization time: ' + str(time() - start))
 #print(data) # OK
+
+dictionary = engine._prepare_dictionary(data)
+# print(dictionary)
+
+corpus = engine._prepare_corpus(data, dictionary)
+# print(corpus[0])
+
+tfidf_model = engine._prepare_tf_idf_model(corpus)
+#print(tfidf_model)
+
+tf_idf_corpus = engine._prepare_tf_idf_corpus(corpus, tfidf_model)
+# print(tf_idf_corpus[0])
+
+print(dir(dictionary))
+print(len(dictionary))
+
+LSI_model = engine._prepare_LSI_model(tf_idf_corpus, dictionary)
+print(dir(LSI_model))
