556c360e54069f4bf1da5172f38b8ade647f6706
PW <pwasows@gmail.com>
2018-06-07 13:22:53 +0200

    Implement AdsDatabase

--- car_recommender/ads_database.py
+++ car_recommender/ads_database.py
@@ -1,9 +1,20 @@
+import pandas as pd
+
+
 class AdsDatabase:
     def __init__(self):
-        pass
+        storage = pd.DataFrame(columns=['hyperlink', 'description', 'tags',
+                                        'active'])
+        self._storage = storage.set_index('hyperlink')
 
     def add_ads(self, ads_data):
-        pass
+        for ad in ads_data:
+            self._storage.loc[ad.hyperlink] = [ad.description, ad.tags, True]
 
     def archive_ads(self, ad_hyperlinks):
-        pass
+        for hyperlink in ad_hyperlinks:
+            self._storage.loc[hyperlink]['active'] = False
+
+    def get_active_ads(self):
+        active_ads = self._storage['active'] == True
+        return self._storage.loc[active_ads]
